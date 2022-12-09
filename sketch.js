f8b89ae5e6ce97d6611481ccedda46952de79ca1
gahyeon11 <117976216+gahyeon11@users.noreply.github.com>
2022-12-09 15:41:23 +0900

    sensor1

---
+++ sketch.js
@@ -0,0 +1,32 @@
+let button;
+let permission = false;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  if( typeof DeviceMotionEvent.requestPermission == "function"){
+    background(255, 0, 0);
+    button = createButton("Click to ios sensor")
+    button.mousePressed(iosAccess); 
+  }else{
+    background(0, 255, 0);
+    text ("is not a ios",100,100);
+
+  }
+}
+
+function iosAccess() {
+  DeviceOrientationEvent.requestPermission()
+.then((response) => {
+if (response == "granted"){
+  ipermission = true;
+                          
+}})
+.catch (console.error);
+}
+
+function draw(){
+  if (!permission) return; 
+  background(255, 255, 255);
+  textSize(72); 
+  text(rotationX, 100, 100);
+} 
